e95d54611e7f9bb1f36ac542ab0d81a3b206fdd9
Potyah <Muline24>
2020-07-13 23:02:32 +0700

    Range FINAL

--- Range/src/ru/academits/potyanikhin/range_main/Main.java
+++ Range/src/ru/academits/potyanikhin/range_main/Main.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        Range range1 = new Range(7, 11);
+        Range range1 = new Range(6, 11);
         Range range2 = new Range(8, 9);
 
         double number = 5.6;
