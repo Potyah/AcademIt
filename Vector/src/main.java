1fb523df6e2a5f57824549a06b79457d2732cab6
Potyah <Muline24>
2020-07-14 23:53:00 +0700

    Vector v.0

--- Vector/src/main.java
+++ Vector/src/main.java
@@ -3,24 +3,30 @@
 import ru.academit.potyanikhin.Vector;
 
 public class main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws CloneNotSupportedException {
 
         double[] array = {5.6, 4, 6.7};
-        double[] array2 = {5.5, 4, 6.5,5.5, 4, 6.5, };
+        double[] array2 = {5.5, 4, 6.5,5.5, 4, 6.5, 8};
 
-        Vector vector = new Vector(7,array);
-        Vector vector1 = new Vector(12, array2);
+        Vector vector = new Vector(array);
+        Vector vector1 = new Vector(array2);
 
-
+        System.out.println (vector);
+        System.out.println (vector1);
 
         Vector vector2 = new Vector(vector1);
 
 
-
+        System.out.println ("сумма: " + vector.vectorSum(vector1));
 
         System.out.println (vector);
         System.out.println (vector1);
-        System.out.println (vector2);
+
+        System.out.println ( "сумма: " + vector1.vectorSum(vector));
+
+
+       // System.out.println (vector.vectorReturn());
+
     }
 
 
--- Vector/src/ru/academit/potyanikhin/Vector.java
+++ Vector/src/ru/academit/potyanikhin/Vector.java
@@ -4,7 +4,7 @@ package ru.academit.potyanikhin;
 import java.util.Arrays;
 
 public class Vector {
-    private double[] vectorArray;
+    private final double[] vectorArray;
     private int n;
 
     public Vector(int n) {
@@ -50,6 +50,32 @@ public class Vector {
     }
 
 
+    public static Vector getVectorSum (double[] vector, double[] vector1) {
+        Vector vectorSum = new Vector(vector);
+
+        for (int i = 0; i < vector1.length; i++) {
+            vectorSum.vectorArray[i] += vector1 [i];
+        }
+
+        return vectorSum;
+    }
+
+    public Vector vectorSum(Vector vector) {
+        if (this.vectorArray.length > vector.vectorArray.length) {
+            return getVectorSum(this.vectorArray, vectorArray);
+
+
+        }
+
+
+        return getVectorSum(vector.vectorArray, this.vectorArray);
+
+    }
+
+
+    public void vectorReturn() {
+        for (int i = 0; i < this.vectorArray.length; i++) {
+            this.vectorArray[i] = vectorArray[i] * (-1);
+        }
+    }
 }
-
-
