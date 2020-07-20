0dccd0c8140eadc1db410b04e88727373dcdc555
Potyah <Apotyanikhin@gmail.com>
2020-07-20 18:25:13 +0700

    Vector v.0

--- Vector/src/main.java
+++ Vector/src/main.java
@@ -4,23 +4,19 @@ public class main {
     public static void main(String[] args) throws CloneNotSupportedException {
 
         double[] array = {5.6, 4, 6.7};
-        double[] array2 = {5.5, 4, 6.5,5.5, 4, 6.5, 8};
+        double[] array2 = {5.5, 4, 6.5, 5.5, 4, 6.5, 8};
 
         Vector vector = new Vector(3, array);
         Vector vector1 = new Vector(7, array2);
 
+        vector.vectorReturn();
 
-        System.out.println (vector);
-        System.out.println (vector1);
+        System.out.println(vector);
+        System.out.println(vector.getVectorLength());
 
-        System.out.println ("сумма: " + vector.vectorSum(vector1));
 
-        System.out.println (vector);
-        System.out.println (vector1);
-
-        System.out.println(vector.vectorDifference(vector1));
+        vector.componentInstallation(99.99, 2);
+        System.out.println(vector);
 
     }
-
-
 }
--- Vector/src/ru/academit/potyanikhin/Vector.java
+++ Vector/src/ru/academit/potyanikhin/Vector.java
@@ -4,17 +4,21 @@ package ru.academit.potyanikhin;
 import java.util.Arrays;
 
 public class Vector implements Cloneable {
+    // Поля
     private double[] vectorArray;
     private int n;
 
+    // Конструктор с нулевыми компонентами;
     public Vector(int n) {
         vectorArray = new double[n];
     }
 
+    // Заполнение вектора значениями из массива;
     public Vector(double[] vectorArray) {
         this.vectorArray = vectorArray;
     }
 
+    // Заполнение вектора значениями из массива. Если длина массива меньше n, то считать что в остальных компонентах 0;
     public Vector(int n, double[] vectorArray) {
         this.vectorArray = new double[n];
 
@@ -23,18 +27,22 @@ public class Vector implements Cloneable {
         }
     }
 
+    // Конструктор копирования;
     public Vector(Vector vector) {
-        this(vector.getVectorArray());
+        this(vector.getVectorArray().clone());
     }
 
+    // Геттер массива компонент;
     public double[] getVectorArray() {
         return vectorArray;
     }
 
+    // Геттер n;
     public int getN() {
         return n;
     }
 
+    // toString;
     @Override
     public String toString() {
         return "Vector{" +
@@ -43,59 +51,130 @@ public class Vector implements Cloneable {
                 '}';
     }
 
+    // Метод для получения размерности вектора;
     public int getSize(Vector vector) {
         return vector.vectorArray.length;
     }
 
-    public Vector vectorSum(Vector vector) {
-        if (this.vectorArray.length > vector.vectorArray.length) {
-            return getVectorSum(this.vectorArray, vector.vectorArray);
+    // Прибавление к вектору другого вектора;
+    public void vectorAddition(Vector vector) {
+        if (this.vectorArray.length < vector.vectorArray.length) {
+            Vector tempVector = new Vector(vector.vectorArray.length, this.vectorArray);
+            this.vectorArray = tempVector.vectorArray;
+
+            for (int i = 0; i < vector.vectorArray.length; i++) {
+                this.vectorArray[i] += vector.vectorArray[i];
+            }
+            return;
         }
 
-        return getVectorSum(vector.vectorArray, this.vectorArray);
+        for (int i = 0; i < this.vectorArray.length; i++) {
+            this.vectorArray[i] += vector.vectorArray[i];
+        }
     }
 
-    private static Vector getVectorSum(double[] vector, double[] vector1) {
-        Vector vectorSum = new Vector(vector.length);
+    // Вычитание из вектора другого вектора;
+    public void vectorDifference(Vector vector) {
+        if (this.vectorArray.length < vector.vectorArray.length) {
+            Vector tempVector = new Vector(vector.vectorArray.length, this.vectorArray);
+            this.vectorArray = tempVector.vectorArray;
 
-        for (int i = 0; i < vector.length; i++) {
-            vectorSum.vectorArray[i] += vector[i];
+            for (int i = 0; i < vector.vectorArray.length; i++) {
+                this.vectorArray[i] -= vector.vectorArray[i];
+            }
+            return;
         }
 
-        for (int i = 0; i < vector1.length; i++) {
-            vectorSum.vectorArray[i] += vector1[i];
+        for (int i = 0; i < this.vectorArray.length; i++) {
+            this.vectorArray[i] -= vector.vectorArray[i];
+        }
+    }
+
+    // Умножение вектора на скаляр;
+    public void scalarMultiplication(double scalar) {
+        for (int i = 0; i < this.vectorArray.length; i++) {
+            this.vectorArray[i] *= scalar;
+        }
+    }
+
+    //Разворот вектора (умножение всех компонент на -1;
+    public void vectorReturn() {
+        for (int i = 0; i < this.vectorArray.length; i++) {
+            this.vectorArray[i] *= (-1);
+        }
+    }
+
+    // Получение длины вектора;
+    public double getVectorLength() {
+        double vectorLength = 0;
+
+        for (int i = 0; i < this.vectorArray.length; i++) {
+            vectorLength += this.vectorArray[i];
+        }
+
+        return Math.abs(vectorLength);
+    }
+
+    //Установка компоненты вектора по индексу;
+    public void componentInstallation(double component, int index) {
+        if (index > this.vectorArray.length) {
+            return;
+        }
+
+        this.vectorArray[index] = component;
+    }
+
+    //Получение компоненты вектора по индексу;
+    public double getComponent(int index) {
+        return this.vectorArray[index];
+
+    }
+
+    //Сложение двух векторов – должен создаваться новый вектор;
+    public static Vector getVectorSum(Vector vector1, Vector vector2) {
+        Vector vectorSum = new Vector(Math.max(vector1.vectorArray.length, vector2.vectorArray.length));
+
+        for (int i = 0; i < vector1.vectorArray.length; i++) {
+            vectorSum.vectorArray[i] += vector1.vectorArray[i];
+        }
+
+        for (int i = 0; i < vector2.vectorArray.length; i++) {
+            vectorSum.vectorArray[i] += vector2.vectorArray[i];
         }
 
         return vectorSum;
     }
 
-    public Vector vectorDifference(Vector vector) {
-        if (this.vectorArray.length < vector.vectorArray.length) {
-            return getVectorDifference(this.vectorArray, vector.vectorArray, vector.vectorArray.length);
+
+    //Вычитание векторов – должен создаваться новый вектор;
+    public static Vector getVectorDifference(Vector vector1, Vector vector2) {
+        Vector vectorDifference = new Vector(Math.max(vector1.vectorArray.length, vector2.vectorArray.length));
+
+        for (int i = 0; i < vector1.vectorArray.length; i++) {
+            vectorDifference.vectorArray[i] += vector1.vectorArray[i];
         }
 
-        return getVectorDifference(this.vectorArray, vector.vectorArray, this.vectorArray.length);
-    }
-
-    public static Vector getVectorDifference(double[] vector1, double[] vector2, int length) {
-        Vector vectorDifference = new Vector(length);
-
-        for (int i = 0; i < vector1.length; i++) {
-            vectorDifference.vectorArray[i] += vector1[i];
-        }
-
-        for (int i = 0; i < vector2.length; i++) {
-            vectorDifference.vectorArray[i] -= vector2[i];
+        for (int i = 0; i < vector2.vectorArray.length; i++) {
+            vectorDifference.vectorArray[i] -= vector2.vectorArray[i];
         }
 
         return vectorDifference;
     }
 
-    public void vectorReturn() {
-        for (int i = 0; i < this.vectorArray.length; i++) {
-            this.vectorArray[i] = vectorArray[i] * (-1);
-        }
-    }
+    // Скалярное произведение векторов;
+    public static Vector getVectorScalarMultiplication(Vector vector1, Vector vector2) {
+        if (vector1.vectorArray.length > vector2.vectorArray.length) {
+            for (int i = 0; i < vector2.vectorArray.length; i++) {
+                vector1.vectorArray[i] *= vector2.vectorArray[i];
+            }
 
-    
+            return vector1;
+        }
+
+        for (int i = 0; i < vector2.vectorArray.length; i++) {
+            vector2.vectorArray[i] *= vector1.vectorArray[i];
+        }
+
+        return vector2;
+    }
 }
