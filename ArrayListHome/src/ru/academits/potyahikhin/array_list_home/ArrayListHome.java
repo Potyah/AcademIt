b7cf61d341e16d9d8499f63d3e152b462a394bee
potyah <Muline24>
2020-08-07 12:42:36 +0700

    ArrayListHome v.1

--- ArrayListHome/src/ru/academits/potyahikhin/array_list_home/ArrayListHome.java
+++ ArrayListHome/src/ru/academits/potyahikhin/array_list_home/ArrayListHome.java
@@ -18,7 +18,42 @@ public class ArrayListHome {
                 Collections.addAll(list, splitString);
             }
 
-            System.out.println(list);
+     //       System.out.println(list);
         }
+
+        ArrayList<Integer> integersList = new ArrayList<>();
+        integersList.add(1);
+        integersList.add(2);
+        integersList.add(3);
+        integersList.add(4);
+        integersList.add(5);
+        integersList.add(6);
+
+        for (int i = 0; i < integersList.size(); i++) {
+            if (integersList.get(i) % 2 == 0) {
+                integersList.remove(i);
+            }
+        }
+
+      //  System.out.println(integersList);
+
+        ArrayList<Integer> integersList2 = new ArrayList<>();
+        integersList2.add(1);
+        integersList2.add(5);
+        integersList2.add(2);
+        integersList2.add(1);
+        integersList2.add(3);
+        integersList2.add(5);
+
+        for (int i = 0; i < integersList2.size(); i++) {
+
+            for (int j = 1; i < integersList2.size() - 1; i++) {
+                if (integersList2.get(i).equals(integersList2.get(j + i))) {
+                    integersList2.remove(j + i);
+                }
+            }
+        }
+
+        System.out.println(integersList2);
     }
 }
